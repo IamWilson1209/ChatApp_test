b70f208e84944298c22e65773ed521019df9c759
李瑋軒 <144474734+IamWilson1209@users.noreply.github.com>
2024-10-09 09:47:20 +0800

    UI design complelte

---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,18 @@
+import Message from "../../../../backend/models/message.model"
+
+const Messages = () => {
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+    </div>
+  )
+}
+
+export default Messages
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "../../components/Sidebar/Sidebar.jsx"
+import MessageContainer from "../../components/messages/MessageContainer.jsx"
+
+const Home = () => {
+  return (
+    <div>
+        <Sidebar />
+        <MessageContainer />
+    </div>
+  )
+}
+
+export default Home
