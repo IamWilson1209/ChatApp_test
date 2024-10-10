a58bd73a8ce1e4bc1f06da0df741bdfe0222ee86
李瑋軒 <144474734+IamWilson1209@users.noreply.github.com>
2024-10-10 22:09:37 +0800

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -21,6 +22,17 @@ app.use(cookieParser()); // Middleware to parse cookies
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+// Get the absolute path of the root directory
+const __dirname = path.resolve();
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// Handle SPA (Single Page Application) routing
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend","dist", "index.html"));
+})
 
 server.listen(PORT, () => {
   connectToMongoDB();
